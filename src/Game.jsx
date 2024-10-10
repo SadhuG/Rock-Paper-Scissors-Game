50968f5ae13c631356bf8bd3ebb66dc61e7c047a
Sadhu <96883913+SadhuG@users.noreply.github.com>
2024-10-11 02:14:49 +0530

    Game completed

--- src/Game.jsx
+++ src/Game.jsx
@@ -3,71 +3,106 @@ import Paper from "./assets/paper.png";
 import Rock from "./assets/rock.png";
 import Scissors from "./assets/scissors.png";
 
-const computerChoice = ["rock", "paper", "scissors"];
+const choices = { rock: Rock, paper: Paper, scissors: Scissors };
 
 function Game() {
 	const [playerWins, setPlayerWins] = useState(0);
 	const [computerWins, setComputerWins] = useState(0);
 	const [ties, setTies] = useState(0);
 
-	function getComputerChoice() {
-		return computerChoice[Math.floor(Math.random() * computerChoice.length)];
-	}
+	const [playerChoice, setPlayerChoice] = useState(null);
+	const [computerChoice, setComputerChoice] = useState(null);
+	const [roundWonBy, setRoundWonBy] = useState(null);
 
-	function playRound(userChose) {
-		const userChoice = userChose;
-		const computerChoice = getComputerChoice();
-		const roundWinner = determineRoundWinner(userChoice, computerChoice);
+	const getRandomChoice = () => {
+		const choicesArray = Object.keys(choices);
+		const randomIndex = Math.floor(Math.random() * choicesArray.length);
+		return choicesArray[randomIndex];
+	};
 
-		console.log(`User Chose: ${userChoice}
-Computer Chose: ${computerChoice}
-Who Won: ${roundWinner}`);
-	}
+	const handleClick = (choice) => {
+		const playerChose = choice;
+		const computerChose = getRandomChoice();
+		setPlayerChoice(playerChose);
+		setComputerChoice(computerChose);
+		const whoWon = playRound(playerChose, computerChose);
+		setRoundWonBy(whoWon);
+	};
 
-	function determineRoundWinner(userChoice, computerChoice) {
-		if (userChoice == computerChoice) {
+	const playRound = (user, computer) => {
+		if (user == computer) {
 			setTies((ties) => ties + 1);
-			return "It's a tie, try again";
-		} else if (userChoice == "rock") {
-			if (computerChoice == "paper") {
+			return "tie";
+		} else if (user == "rock") {
+			if (computer == "paper") {
 				setComputerWins((computerWins) => computerWins + 1);
-				return "Computer Wins";
-			} else if (computerChoice == "scissors") {
+				return "computer";
+			} else if (computer == "scissors") {
 				setPlayerWins((playerWins) => playerWins + 1);
-				return "User Wins";
+				return "player";
 			}
-		} else if (userChoice == "paper") {
-			if (computerChoice == "scissors") {
+		} else if (user == "paper") {
+			if (computer == "scissors") {
 				setComputerWins((computerWins) => computerWins + 1);
 
-				return "Computer Wins";
-			} else if (computerChoice == "rock") {
+				return "computer";
+			} else if (computer == "rock") {
 				setPlayerWins((playerWins) => playerWins + 1);
-				return "User Wins";
+				return "player";
 			}
-		} else if (userChoice == "scissors") {
-			if (computerChoice == "rock") {
+		} else if (user == "scissors") {
+			if (computer == "rock") {
 				setComputerWins((computerWins) => computerWins + 1);
 
-				return "Computer Wins";
-			} else if (computerChoice == "paper") {
+				return "computer";
+			} else if (computer == "paper") {
 				setPlayerWins((playerWins) => playerWins + 1);
-				return "User Wins";
+				return "player";
 			}
 		}
-	}
+	};
 
 	return (
 		<section className="h-screen flex flex-col justify-center items-center gap-8 max-[500px]:gap-4">
 			<div className="flex items-center max-[500px]:flex-col gap-8 max-[500px]:gap-4">
 				<div className="flex flex-col items-center">
-					<div className="w-40 h-40 border-4 border-zinc-400 rounded-3xl"></div>
+					<div
+						className={`w-40 h-40 border-8 border-zinc-400 ${
+							roundWonBy === "player" ? "border-green-500" : ""
+						} ${roundWonBy === "tie" ? "border-amber-500" : ""} ${
+							roundWonBy && roundWonBy !== "player" && roundWonBy !== "tie"
+								? "border-red-400"
+								: ""
+						} rounded-3xl`}
+					>
+						{playerChoice && (
+							<img
+								src={choices[playerChoice]}
+								alt={playerChoice}
+							/>
+						)}
+					</div>
 					<p>Player</p>
 				</div>
 
 				<p className="text-neutral-50">v/s</p>
 				<div className="flex flex-col items-center">
-					<div className="w-40 h-40 border-4 border-zinc-400 rounded-3xl"></div>
+					<div
+						className={`w-40 h-40 border-8 border-zinc-400 ${
+							roundWonBy === "computer" ? "border-green-500" : ""
+						} ${roundWonBy === "tie" ? "border-amber-500" : ""} ${
+							roundWonBy && roundWonBy !== "computer" && roundWonBy !== "tie"
+								? "border-red-400"
+								: ""
+						} rounded-3xl`}
+					>
+						{computerChoice && (
+							<img
+								src={choices[computerChoice]}
+								alt={computerChoice}
+							/>
+						)}
+					</div>
 					<p>Computer</p>
 				</div>
 			</div>
@@ -76,9 +111,9 @@ Who Won: ${roundWinner}`);
 			<div className="flex gap-8">
 				<button
 					onClick={() => {
-						playRound("rock");
+						handleClick("rock");
 					}}
-					className="w-16 h-16 border-8 border-orange-400 bg-orange-400/25 rounded-full p-2"
+					className="w-20 h-20 border-8 border-orange-400 bg-orange-400/25 rounded-full p-2"
 				>
 					<img
 						src={Rock}
@@ -88,9 +123,9 @@ Who Won: ${roundWinner}`);
 
 				<button
 					onClick={() => {
-						playRound("paper");
+						handleClick("paper");
 					}}
-					className="w-16 h-16 border-8 border-teal-400 bg-teal-400/25 rounded-full p-2 "
+					className="w-20 h-20 border-8 border-teal-400 bg-teal-400/25 rounded-full p-2 "
 				>
 					<img
 						src={Paper}
@@ -100,9 +135,9 @@ Who Won: ${roundWinner}`);
 
 				<button
 					onClick={() => {
-						playRound("scissors");
+						handleClick("scissors");
 					}}
-					className="w-16 h-16 border-8 border-violet-400 bg-violet-400/25 rounded-full p-2 "
+					className="w-20 h-20 border-8 border-violet-400 bg-violet-400/25 rounded-full p-2 "
 				>
 					<img
 						src={Scissors}
