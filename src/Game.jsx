3525755eb6aa5c14aacaac21c089fd734987e034
Sadhu <96883913+SadhuG@users.noreply.github.com>
2024-10-11 00:00:39 +0530

    console results from the ui

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,16 +1,14 @@
 import { useState } from "react";
-
-const userChoice = [
-	{ value: "rock", text: "rock 🪨" },
-	{ value: "paper", text: "paper 📃" },
-	{ value: "scissors", text: "scissors ✂️" },
-];
+import Paper from "./assets/paper.png";
+import Rock from "./assets/rock.png";
+import Scissors from "./assets/scissors.png";
 
 const computerChoice = ["rock", "paper", "scissors"];
 
 function Game() {
 	const [playerWins, setPlayerWins] = useState(0);
 	const [computerWins, setComputerWins] = useState(0);
+	const [ties, setTies] = useState(0);
 
 	function getComputerChoice() {
 		return computerChoice[Math.floor(Math.random() * computerChoice.length)];
@@ -28,6 +26,7 @@ Who Won: ${roundWinner}`);
 
 	function determineRoundWinner(userChoice, computerChoice) {
 		if (userChoice == computerChoice) {
+			setTies((ties) => ties + 1);
 			return "It's a tie, try again";
 		} else if (userChoice == "rock") {
 			if (computerChoice == "paper") {
@@ -59,24 +58,70 @@ Who Won: ${roundWinner}`);
 	}
 
 	return (
-		<section>
-			<div>
-				{userChoice.map((choice) => {
-					return (
-						<button
-							key={choice.value}
-							value={choice.value}
-							onClick={(e) => {
-								playRound(e.target.value);
-							}}
-						>
-							{choice.text}
-						</button>
-					);
-				})}
+		<section className="h-screen flex flex-col justify-center items-center gap-8 max-[500px]:gap-4">
+			<div className="flex items-center max-[500px]:flex-col gap-8 max-[500px]:gap-4">
+				<div className="flex flex-col items-center">
+					<div className="w-40 h-40 border-4 border-zinc-400 rounded-3xl"></div>
+					<p>Player</p>
+				</div>
+
+				<p className="text-neutral-50">v/s</p>
+				<div className="flex flex-col items-center">
+					<div className="w-40 h-40 border-4 border-zinc-400 rounded-3xl"></div>
+					<p>Computer</p>
+				</div>
 			</div>
-			Player Won: {playerWins}
-			Computer Won: {computerWins}
+
+			{/* User Input Buttons */}
+			<div className="flex gap-8">
+				<button
+					onClick={() => {
+						playRound("rock");
+					}}
+					className="w-16 h-16 border-8 border-orange-400 bg-orange-400/25 rounded-full p-2"
+				>
+					<img
+						src={Rock}
+						alt=""
+					/>
+				</button>
+
+				<button
+					onClick={() => {
+						playRound("paper");
+					}}
+					className="w-16 h-16 border-8 border-teal-400 bg-teal-400/25 rounded-full p-2 "
+				>
+					<img
+						src={Paper}
+						alt=""
+					/>
+				</button>
+
+				<button
+					onClick={() => {
+						playRound("scissors");
+					}}
+					className="w-16 h-16 border-8 border-violet-400 bg-violet-400/25 rounded-full p-2 "
+				>
+					<img
+						src={Scissors}
+						alt=""
+					/>
+				</button>
+			</div>
+
+			<div className="flex flex-col items-center">
+				<p>
+					Is a Tie: <span className="text-neutral-50">{ties}</span>
+				</p>
+				<p>
+					Player Won: <span className="text-neutral-50">{playerWins}</span>
+				</p>
+				<p>
+					Computer Won: <span className="text-neutral-50">{computerWins}</span>
+				</p>
+			</div>
 		</section>
 	);
 }
