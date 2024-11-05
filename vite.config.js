3291b93385f3b8385f84e5db7216b53228d665d4
Sadhu <96883913+SadhuG@users.noreply.github.com>
2024-11-05 15:01:41 +0530

    Manually updated package names

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/RockPaperScissorsGame",
+	base: "/Rock-Paper-Scissors-Game",
 	plugins: [react()],
 });
