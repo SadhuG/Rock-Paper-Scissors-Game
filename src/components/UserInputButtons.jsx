73a1c8603780aa8cee92d9c06b7c29526445d297
Sadhu <96883913+SadhuG@users.noreply.github.com>
2024-12-18 00:37:11 +0530

    Added images and styling to user input buttons

--- src/components/UserInputButtons.jsx
+++ src/components/UserInputButtons.jsx
@@ -1,32 +1,51 @@
 import PropTypes from "prop-types";
+
+import Paper from "../assets/paper.png";
+import Rock from "../assets/rock.png";
+import Scissors from "../assets/scissors.png";
+
 const UserInputButtons = ({ inputDisabled, handleUserInput }) => {
 	return (
-		<div>
+		<div className="flex gap-8">
 			<button
 				disabled={inputDisabled}
 				onClick={() => handleUserInput("rock")}
+				className="w-20 h-20 border-8 border-teal-400 bg-teal-400/25 rounded-full p-2"
 			>
-				Rock
+				<img
+					src={Rock}
+					alt="Rock Button"
+				/>
 			</button>
 
 			<button
 				disabled={inputDisabled}
 				onClick={() => handleUserInput("paper")}
+				className="w-20 h-20 border-8 border-sky-400 bg-sky-400/25 rounded-full p-2 "
 			>
-				Paper
+				<img
+					src={Paper}
+					alt="Paper Button"
+				/>
 			</button>
 
 			<button
 				disabled={inputDisabled}
 				onClick={() => handleUserInput("scissors")}
+				className="w-20 h-20 border-8 border-indigo-400 bg-indigo-400/25 rounded-full p-2 "
 			>
-				Scissors
+				<img
+					src={Scissors}
+					alt="Scissors Button"
+				/>
 			</button>
 		</div>
 	);
 };
+
 UserInputButtons.propTypes = {
 	inputDisabled: PropTypes.bool,
 	handleUserInput: PropTypes.func,
 };
+
 export default UserInputButtons;
