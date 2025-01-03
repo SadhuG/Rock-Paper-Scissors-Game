02b92e1c6fe184bfb52ba2d076c8664e08c6944d
Sadhu <96883913+SadhuG@users.noreply.github.com>
2025-01-03 23:52:21 +0530

    Bug fixes

--- src/components/RulesButtonAndModal.jsx
+++ src/components/RulesButtonAndModal.jsx
@@ -39,64 +39,67 @@ const RulesButtonAndModal = () => {
 				className="w-full max-w-full h-full max-h-full p-4 md:p-8 lg:py-10 lg:px-32 bg-black/30 fixed top-0 left-0 m-0"
 			>
 				<div className="w-full h-full max-w-full max-h-full flex items-start justify-center">
-					<div className="w-full h-full max-w-full max-h-full overflow-scroll flex flex-col gap-6 items-start bg-slate-900 text-xl lg:text-2xl text-white/90 px-3 md:px-10 py-10 md:py-14 border border-zinc-400 rounded-2xl">
-						<div className="flex justify-between w-full">
-							<p className="font-medium text-3xl">Rules:</p>
-							<div
-								onClick={handleExit}
-								className="focus:outline-none text-white bg-rose-600 hover:bg-rose-800 focus:ring-4 focus:ring-rose-300  rounded-lg text-xl lg:text-2xl font-medium px-5 py-1 dark:bg-rose-500 dark:hover:bg-rose-700 dark:focus:ring-rose-900"
-							>
-								<p>X</p>
+					<div className="w-full h-full max-w-full max-h-full overflow-hidden flex flex-col gap-6 items-start  bg-slate-900 text-xl lg:text-2xl text-white/90  border border-zinc-400 rounded-2xl">
+						<div className="w-full h-full max-w-full max-h-full flex flex-col gap-6 items-start  overflow-y-auto px-3 md:px-10 py-10 md:py-14">
+							<div className="flex justify-between w-full">
+								<p className="font-medium text-3xl">Rules:</p>
+								<div
+									onClick={handleExit}
+									className="focus:outline-none text-white bg-rose-600 hover:bg-rose-800 focus:ring-4 focus:ring-rose-300  rounded-lg text-xl lg:text-2xl font-medium px-5 py-1 dark:bg-rose-500 dark:hover:bg-rose-700 dark:focus:ring-rose-900"
+								>
+									<p>X</p>
+								</div>
 							</div>
-						</div>
-						<div className="flex flex-col gap-2 items-start">
-							<p className="font-medium text-2xl lg:text-3xl">What is this? </p>
-							<p>
-								This is a rock paper scissors game. Built with React JS &
-								Tailwind CSS framework.
-							</p>
-						</div>
-
-						<div className="flex flex-col gap-2 items-start">
-							<p className="font-medium text-2xl lg:text-3xl">How to play</p>
-
-							<div className="flex flex-col gap-3 items-start">
-								<p>
-									Choose the game mode:{" "}
-									<span className="font-medium">Rounds</span> or{" "}
-									<span className="font-medium">Pass n Play</span>
+							<div className="flex flex-col gap-2 items-start">
+								<p className="font-medium text-2xl lg:text-3xl">
+									What is this?{" "}
 								</p>
-								<ul className="list-decimal list-inside flex flex-col gap-2 items-start">
+								<p>
+									This is a rock paper scissors game. Built with React JS &
+									Tailwind CSS framework.
+								</p>
+							</div>
+
+							<div className="flex flex-col gap-2 items-start">
+								<p className="font-medium text-2xl lg:text-3xl">How to play</p>
+
+								<div className="flex flex-col gap-3 items-start">
+									<p>
+										Choose the game mode:{" "}
+										<span className="font-medium">Rounds</span> or{" "}
+										<span className="font-medium">Pass n Play</span>
+									</p>
+									<ul className="list-decimal list-inside flex flex-col gap-2 items-start">
+										<li>
+											<span className="font-medium text-wrap">Rounds: </span>
+											Start playing without any specific number of rounds input
+											and play for as long as you want (upto 100 rounds)
+										</li>
+										<li>
+											<span className="font-medium">Pass n play: </span>
+											Start playing without any specific number of rounds input
+											and play for as long as you want (upto 100 rounds)
+										</li>
+									</ul>
+								</div>
+
+								<ul className="list-disc list-inside flex flex-col gap-2 items-start">
 									<li>
-										<span className="font-medium text-wrap">Rounds: </span>
-										Start playing without any specific number of rounds input
-										and play for as long as you want (upto 100 rounds)
+										You can start the game by selecting "rock", "scissors" or
+										"paper".
 									</li>
 									<li>
-										<span className="font-medium">Pass n play: </span>
-										Start playing without any specific number of rounds input
-										and play for as long as you want (upto 100 rounds)
+										Rock beats scissors, scissors beats paper, paper beats rock
 									</li>
 								</ul>
 							</div>
-
-							<ul className="list-disc list-inside flex flex-col gap-2 items-start">
-								<li>
-									You can start the game by selecting "rock", "scissors" or
-									"paper".
-								</li>
-								<li>
-									Rock beats scissors, scissors beats paper, paper beats rock
-								</li>
-							</ul>
+							<button
+								onClick={handleExit}
+								className="focus:outline-none text-white bg-rose-600 hover:bg-rose-800 focus:ring-4 focus:ring-rose-300  rounded-lg text-xl lg:text-2xl font-medium px-5 py-2.5 dark:bg-rose-500 dark:hover:bg-rose-700 dark:focus:ring-rose-900"
+							>
+								Close
+							</button>
 						</div>
-
-						<button
-							onClick={handleExit}
-							className="focus:outline-none text-white bg-rose-600 hover:bg-rose-800 focus:ring-4 focus:ring-rose-300  rounded-lg text-xl lg:text-2xl font-medium px-5 py-2.5 dark:bg-rose-500 dark:hover:bg-rose-700 dark:focus:ring-rose-900"
-						>
-							Close
-						</button>
 					</div>
 				</div>
 			</dialog>
