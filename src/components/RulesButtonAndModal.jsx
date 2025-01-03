315f350c94774ea2d986e3a4a8187ac6ae274c5e
Sadhu <96883913+SadhuG@users.noreply.github.com>
2025-01-03 23:26:31 +0530

    Bug Fixes

--- src/components/RulesButtonAndModal.jsx
+++ src/components/RulesButtonAndModal.jsx
@@ -38,12 +38,15 @@ const RulesButtonAndModal = () => {
 				ref={rulesModal}
 				className="w-full max-w-full h-full max-h-full p-4 md:p-8 lg:py-10 lg:px-32 bg-black/30 fixed top-0 left-0 m-0"
 			>
-				<div className="w-full h-full max-w-full max-h-full flex items-center justify-center">
-					<div className="w-full h-full max-w-full max-h-full overflow-scroll flex flex-col gap-6 items-center bg-slate-900 text-xl lg:text-2xl text-white/90 px-3 md:px-10 py-10 md:py-14 border border-zinc-400 rounded-2xl">
+				<div className="w-full h-full max-w-full max-h-full flex items-start justify-center">
+					<div className="w-full h-full max-w-full max-h-full overflow-scroll flex flex-col gap-6 items-start bg-slate-900 text-xl lg:text-2xl text-white/90 px-3 md:px-10 py-10 md:py-14 border border-zinc-400 rounded-2xl">
 						<div className="flex justify-between w-full">
 							<p className="font-medium text-3xl">Rules:</p>
-							<div className="focus:outline-none text-white bg-rose-600 hover:bg-rose-800 focus:ring-4 focus:ring-rose-300  rounded-lg text-xl lg:text-2xl font-medium px-5 py-1 dark:bg-rose-500 dark:hover:bg-rose-700 dark:focus:ring-rose-900">
-								<p onClick={handleExit}>X</p>
+							<div
+								onClick={handleExit}
+								className="focus:outline-none text-white bg-rose-600 hover:bg-rose-800 focus:ring-4 focus:ring-rose-300  rounded-lg text-xl lg:text-2xl font-medium px-5 py-1 dark:bg-rose-500 dark:hover:bg-rose-700 dark:focus:ring-rose-900"
+							>
+								<p>X</p>
 							</div>
 						</div>
 						<div className="flex flex-col gap-2 items-start">
